9646ad7614d9189f4ade4ade4ece2eddeb6c2d2d
Naveen V <121096459+naveenv0906@users.noreply.github.com>
2026-06-05 12:18:29 +0530

    booksdb.js file commiting

---
+++ router/booksdb.js
@@ -0,0 +1,23 @@
+// booksdb.js
+let books = {
+  "1": {
+    isbn: "1",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    reviews: {}
+  },
+  "2": {
+    isbn: "2",
+    title: "1984",
+    author: "George Orwell",
+    reviews: {}
+  },
+  "3": {
+    isbn: "3",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    reviews: {}
+  }
+};
+
+module.exports = books;
