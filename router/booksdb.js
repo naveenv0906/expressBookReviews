a114b8b2f2af6aad22153f6b9969ee867a1c0b2d
Naveen V <121096459+naveenv0906@users.noreply.github.com>
2026-06-05 12:59:50 +0530

    booksdb data change

--- router/booksdb.js
+++ router/booksdb.js
@@ -17,6 +17,48 @@ let books = {
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
     reviews: {}
+  },
+  "4": {
+    isbn: "4",
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    reviews: {}
+  },
+  "5": {
+    isbn: "5",
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    reviews: {}
+  },
+  "6": {
+    isbn: "6",
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    reviews: {}
+  },
+  "7": {
+    isbn: "7",
+    title: "Moby Dick",
+    author: "Herman Melville",
+    reviews: {}
+  },
+  "8": {
+    isbn: "8",
+    title: "War and Peace",
+    author: "Leo Tolstoy",
+    reviews: {}
+  },
+  "9": {
+    isbn: "9",
+    title: "The Odyssey",
+    author: "Homer",
+    reviews: {}
+  },
+  "10": {
+    isbn: "10",
+    title: "Jane Eyre",
+    author: "Charlotte Bronte",
+    reviews: {}
   }
 };
 
